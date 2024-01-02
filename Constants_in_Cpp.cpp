c7075cd6b8799c8f2bc894599c085ab298f2848e
Wafa Ayan <wafaayankhan@gmail.com>
2024-01-02 11:35:03 +0500

    Constants in cpp file uploaded

---
+++ Constants_in_Cpp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main(){
+
+const int number = 10;
+const float pi = 3.14;
+const char dollar = '$';
+const string myName = "Wafa Ayan";
+
+cout << number << endl;
+cout << pi << endl;
+cout << dollar << endl;
+cout << myName << endl;
+
+return 0;
+
+}
